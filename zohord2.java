eea54fb52d803ade06a2ba7ff17e0c5ac5466d71
Surya V <71581602+Surya-V07@users.noreply.github.com>
2021-04-15 10:20:22 +0530

    Update zohord2.java

--- zohord2.java
+++ zohord2.java
@@ -1,8 +1,7 @@
-package zoho;
 
 import java.util.Scanner;
 
-public class zohord2 {
+public class Main{
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
 		String s=sc.nextLine();
